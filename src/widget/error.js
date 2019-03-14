d947fcb41d72b0eda8e959618cf7e52582718b56
葉岬 <XGLey@protonmail.com>
2019-03-14 19:52:31 +0800

    大胆地用上sanctuary。

--- src/widget/error.js
+++ src/widget/error.js
@@ -1,5 +1,9 @@
+const R = require("ramda");
+const S = require("sanctuary");
 const Most = require("most");
 const dom = require("@cycle/dom");
+
+const { fmap } = require("XGLib/ext");
 
 const drawError = msg => dom.div(".ui.negative.message", [
 	dom.div(".header", "出现可怕的错误！"),
@@ -12,4 +16,15 @@ const errorMsg = e => {
 	return Most.of(drawError(e.message));
 };
 
+// renderMaybe :: Maybe View -> View
+const renderMaybe = S.maybeToNullable;
+
+// renderEither :: Either String View -> View
+const renderEither = S.either(
+	msg => dom.div(".ui.red.form", dom.p(msg)),
+	S.I
+);
+
 exports.errorMsg = errorMsg;
+exports.renderMaybe = renderMaybe;
+exports.renderEither = renderEither;
