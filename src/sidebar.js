22c4dc6d9441916dffb8d4bf591784ca4c7e6bc0
荀徒之 <kalxd@126.com>
2020-01-04 15:03:51 +0800

    显示信息不完整页面！

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,11 +1,54 @@
+const Most = require("most");
+const R = require("ramda");
+const dom = require("@cycle/dom");
+
 const { runAtApp } = require("XGWidget/run");
 const AppState = require("XGState/app");
 
 const SidebarW = require("./sidebar/index");
 
+// renderError :: Error -> View
+const renderError = ({ message }) => dom.div(".ui.negative.message", [
+	dom.i(".close.icon"),
+	dom.div(".header", message),
+	dom.div([
+		dom.button(".ui.mini.red.button.__go-option__", "马上去登记！")
+	])
+]);
+
 // main :: Source -> Application
 const main = source => {
-	return SidebarW(source, AppState.读取选项());
+	// 打开选项页。
+	source.DOM$.select(".__go-option__").events("click")
+		.observe(_ => browser.runtime.openOptionsPage())
+	;
+
+	// appState$ :: Stream AppState
+	const appState$ = AppState.读取选项()
+		.concatMap(state => {
+			if (R.isNil(state)) {
+				const e = new Error("信息不完整！");
+				return Most.throwError(e);
+			}
+			else {
+				return Most.of(state);
+			}
+		})
+	;
+
+	const sidebarApp = SidebarW(source, appState$);
+
+	const DOM$ = sidebarApp.DOM$
+		.recoverWith(R.compose(
+			Most.of,
+			renderError
+		))
+	;
+
+	return {
+		DOM$,
+		state: sidebarApp.state
+	};
 };
 
 runAtApp(main);
--- src/sidebar/index.js
+++ src/sidebar/index.js
@@ -6,9 +6,10 @@ const LoadState = require("XGState/load");
 
 const LoadV = require("./view/load");
 
-// main :: Source -> Stream (Maybe AppState) -> Application
+// main :: Source -> Stream AppState -> Application
 const main = (source, appState$) => {
-	const DOM$ = Most.of(LoadState.empty)
+	const DOM$ = appState$
+		.constant(LoadState.empty)
 		.map(LoadV.render)
 	;
 
