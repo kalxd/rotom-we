1901e36df59c5956abf8f2ce3bd312e9157fbcc5
荀徒之 <kalxd@126.com>
2020-01-06 19:40:36 +0800

    更改布局。

--- src/sidebar/render.js
+++ src/sidebar/render.js
@@ -1,14 +1,35 @@
+const R = require("ramda");
 const dom = require("@cycle/dom");
+const State = require("./state");
+
+const { fmap } = require("XGLib/ext");
+
+// renderDropdown :: View -> View
+const renderDropdown = dropdownDOM => dom.div(".ui.segment", [
+	dom.div(".ui.form", [
+		dom.div(".inline.fields", [
+			dom.label("选择一个分类"),
+			dom.div(".field", [dropdownDOM])
+		])
+	])
+])
 
 // render :: (SidebarState, View) -> View
 const render = ([state, dropdownDOM]) => {
-	return dom.div(".ui.mini.menu", [
-		dom.div(".item", dropdownDOM),
-		dom.div(".item", [
-			dom.button(".ui.primary.button.__add-group__", "添加分组")
-		]),
-		dom.div(".item", [
-			dom.button(".ui.green.button.__add-emoji__", "添加表情")
+	const 当前分组 = R.view(State.选中分组lens, state);
+
+	return dom.div(".ui.stacked.segments", [
+		renderDropdown(dropdownDOM),
+
+		dom.div(".ui.segment", [
+			dom.button(".ui.primary.button.__add-group__", "添加分组"),
+			fmap(
+				_ => dom.button(".ui.orange.button.__edit-group__", "编辑分组")
+			)(当前分组),
+
+			fmap(
+				_ => dom.button(".ui.red.button.__delete-group__", "删除分组")
+			)(当前分组)
 		])
 	]);
 };
